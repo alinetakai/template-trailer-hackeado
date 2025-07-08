cb73b7cc443097a8c0ea7d3849cc426fab69ac97
Aline T. de Araújo Thomaz <projetosocialinfo@gmail.com>
2025-07-08 01:45:30 -0300

    Create trailer-hackeado.py

---
+++ trailer-hackeado.py
@@ -0,0 +1,7 @@
+sinopse = """
+[0001]::|>✨Prepare-se para uma aventura épica no espaço profundo!<|::[fim]
+[0002]::|>✨Personagens cativantes e cenas de tirar o fôlego te aguardam.<|::[fim]
+[0003]::|>✨Estreia mundial em breve. Você não pode perder!<|::[fim]
+"""
+
+# Digite seu código abaixo:
